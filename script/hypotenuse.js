eb3e21a8e2f48a0b05c16ed5b9bad2fed494d70b
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2021-09-27 21:28:11 +0530

    processing

---
+++ script/hypotenuse.js
@@ -0,0 +1,19 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const ouputE1 = document.querySelector("#output");
+
+function calculateSumOfSquares(a, b) {
+  const sumOfSquares = a * a + b * b;
+  return sumOfSquares;
+}
+
+function calculateHypotenuse() {
+  const sumOfSquares = calculateSumOfSquares(
+    Number(sides[0].value),
+    Number(sides[1].value)
+  );
+  const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+  ouputE1.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse;
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
